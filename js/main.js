9184521b8411836cd5e3cb73d90bd4def02375eb
bekzodxudaybergenow <bekzodxudaybergenow@gmail.com>
2023-12-30 18:13:55 +0500

    Add the javascript part of the Images section

--- js/main.js
+++ js/main.js
@@ -6,3 +6,17 @@ sidebarBtn.addEventListener('click', () => {
     sidebarBtn.classList.toggle('active');
     headerMenuNav.classList.toggle('header__menu--sidebar');
 })
+
+
+let swiper = new Swiper(".images__swiper.swiper", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 8500,
+    disableOnInteraction: false
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  },
+});
